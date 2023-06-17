a289d4c5e6764af2165d7d6186df98e800f1f6f4
Jefferson Alves <jeeffsantoos@outlook.com>
2023-06-17 13:44:55 -0300

    build: Configuração dos Services (Axios e History)

---
+++ src/services/axios.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://localhost:3030",
+});
---
+++ src/services/history.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+
+export default history;
